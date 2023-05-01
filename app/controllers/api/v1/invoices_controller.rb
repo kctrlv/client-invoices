2be8ce69071bbd4795e29c5e1ac9f6bce3b05e63
David <kctrlv@gmail.com>
2023-05-01 10:41:01 -0400

    user can update invoice status

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::InvoicesController < ActionController::API
+  before_action :set_invoice, only: [:show, :update]
+
+  def show
+    render json: @invoice
+  end
+
   def create
     @client = Client.find(params[:client_id])
     @invoice = @client.invoices.build(invoice_params)
@@ -11,9 +17,27 @@ class Api::V1::InvoicesController < ActionController::API
     end
   end
 
+  def update
+    @client = Client.find(params[:client_id])
+    @invoice = @client.invoices.find(params[:id])
+    
+    if @invoice.status == 'closed'
+      render json: { error: 'Invoice is already closed.' }, status: :unprocessable_entity
+    elsif @invoice.update(invoice_params)
+      render json: @invoice
+    else
+      render json: @invoice.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
+  def set_invoice
+    @client = Client.find(params[:client_id])
+    @invoice = @client.invoices.find(params[:id])
+  end
+
   def invoice_params
-    params.require(:invoice).permit(:number, :amount, :due_date)
+    params.require(:invoice).permit(:number, :amount, :due_date, :status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :clients, only: [:create, :index, :show] do
-        resources :invoices, only: [:create]
+        resources :invoices, only: [:create, :update, :show]
         member do
           get :invoices
         end  
